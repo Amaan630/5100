27013c4b5bc94504e82be6c80d4179f1fe88beb7
Hitesh Yarlagadda <hiteshyarlagadda17@gmail.com>
2024-11-25 19:48:34 -0500

    updated input

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_hashtags():
+    user_tweet = request.form.get('tweet', '')
+    hashtags = ['#Example', '#Hashtag', '#Generation']  # Placeholder for hashtag generation logic
+    return render_template('index.html', tweet=user_tweet, hashtags=hashtags)
 
 if __name__ == '__main__':
     app.run(debug=True)
